24f9eb38b6a9834c9937033a68a55ec551915e75
TUDOR-CRISTIAN COBLIȘ <tudor.coblis@stud.ubbcluj.ro>
2023-10-21 22:38:35 +0300

    Add project files.

---
+++ Data/Coblis_TudorCristian_Lab2Context.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Coblis_TudorCristian_Lab2.Models;
+
+namespace Coblis_TudorCristian_Lab2.Data
+{
+    public class Coblis_TudorCristian_Lab2Context : DbContext
+    {
+        public Coblis_TudorCristian_Lab2Context (DbContextOptions<Coblis_TudorCristian_Lab2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Coblis_TudorCristian_Lab2.Models.Book> Book { get; set; } = default!;
+
+        public DbSet<Coblis_TudorCristian_Lab2.Models.Publisher>? Publisher { get; set; }
+    }
+}
