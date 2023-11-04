dd1305d66d1f0f04f5f6af3d0a877f3f9d4f9677
TUDOR-CRISTIAN COBLIȘ <tudor.coblis@stud.ubbcluj.ro>
2023-11-04 23:34:09 +0200

    Laborator 4 complet.

--- Pages/Categories/Index.cshtml.cs
+++ Pages/Categories/Index.cshtml.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using Coblis_TudorCristian_Lab2.Data;
 using Coblis_TudorCristian_Lab2.Models;
+using Coblis_TudorCristian_Lab2.Models.ViewModels;
 
 namespace Coblis_TudorCristian_Lab2.Pages.Categories
 {
@@ -21,11 +22,23 @@ namespace Coblis_TudorCristian_Lab2.Pages.Categories
 
         public IList<Category> Category { get;set; } = default!;
 
-        public async Task OnGetAsync()
+        public CategoryIndexData CategoryData { get; set; }
+        public int CategoryID { get; set; }
+        public int BookID { get; set; }
+        public async Task OnGetAsync(int? id, int? bookID)
         {
-            if (_context.Category != null)
+            CategoryData = new CategoryIndexData();
+            CategoryData.Categories = await _context.Category
+            .Include(i => i.BookCategories)
+            .ThenInclude(c => c.Book.Author)
+            .OrderBy(i => i.CategoryName)
+            .ToListAsync();
+            if (id != null)
             {
-                Category = await _context.Category.ToListAsync();
+                CategoryID = id.Value;
+                Category category = CategoryData.Categories
+                .Where(i => i.ID == id.Value).Single();
+                CategoryData.Books = category.BookCategories.Select(c => c.Book);
             }
         }
     }
