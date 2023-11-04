beb8e3dfffb9ec511e88fb3fc606591e4f8b1be9
TUDOR-CRISTIAN COBLIȘ <tudor.coblis@stud.ubbcluj.ro>
2023-11-04 18:46:04 +0200

    Laborator facut dupa instructiuni.

---
+++ Models/ViewModels/PublisherIndexData.cs
@@ -0,0 +1,10 @@
+﻿using Coblis_TudorCristian_Lab2.Models;
+
+namespace Coblis_TudorCristian_Lab2.Models.ViewModels
+{
+    public class PublisherIndexData
+    {
+        public IEnumerable<Publisher> Publishers { get; set; }
+        public IEnumerable<Book> Books { get; set; }
+    }
+}
